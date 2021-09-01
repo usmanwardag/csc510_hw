3bab9a0a8738700bf744c408668c66c9f67b01b3
Usman <usmanwardag@gmail.com>
2021-08-31 20:17:52 -0400

    Setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='hwcsc510',
+    version='0.1',
+    description='Module fow HW2 CSC-510',
+    author='Usman Khan',
+    author_email='ukhan3@ncsu.edu',
+    packages=['hwcsc510'],
+      long_description="""\
+        Module fow HW2 CSC-510
+            * Test Cases 
+            * Mock Dataset 
+            * Mock Code 
+      """,
+      classifiers=[
+          "License :: MIT License", 
+          "Programming Language :: Python",
+          "Intended Audience :: Developers",
+      ],
+      license='MIT',
+      #install_requires=[
+      #  'numpy',
+      #  'scipy',
+      #],
+      )
