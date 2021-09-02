fbbecad4e98b4c4be0dcec6af42edc701cf16a00
sbosenc <89551210+sbosenc@users.noreply.github.com>
2021-09-01 20:04:07 -0400

    added unit test cases

--- test/starbucks_drinks_test.py
+++ test/starbucks_drinks_test.py
@@ -1,2 +1,35 @@
-def test_answer():
-    assert inc(3) == 5
+import unittest
+import pandas as pd
+from pandas.util.testing import assert_frame_equal 
+
+class TestDrinks(unittest.TestCase):
+    
+    def test_lowest_calorie(self):
+        print('Hello')
+        #Assume
+        test_data = pd.read_csv('test-data.csv')
+        data = {'Name':'Drink 5', 'Calories':0, 'Fat (g)':0, 'Carb. (g)':0, 'Fiber (g)':0 , 'Protein':5, 'Sodium':0}
+        expected = pd.DataFrame(data)
+        
+        #Assert
+        assert_frame_equal(expected,find_low_calorie_drink(test_data))
+        
+    def test_high_protein(self):
+        #Assume
+        test_data = pd.read_csv('test-data.csv')
+        data = {'Name':['Drink 1,Drink 5'], 'Calories':[45,0], 'Fat (g)':[0,0], 'Carb. (g)':[11,0], 'Fiber (g)':[0,0] , 'Protein':[5,5], 'Sodium':[10,0]}
+        expected = pd.DataFrame(data)
+        
+        #Assert
+        assert_frame_equal(expected,find_high_protein_drink(test_data))
+        
+    def test_high_carb(self):
+        #Assume
+        test_data = pd.read_csv('test-data.csv')
+        data = {'Name':'Drink 3', 'Calories':60, 'Fat (g)':0, 'Carb. (g)':34, 'Fiber (g)':1 , 'Protein':1, 'Sodium':10}
+        expected = pd.DataFrame(data)
+        
+        #Assert
+        assert_frame_equal(expected,find_high_carb_drink(test_data))
+        
+
