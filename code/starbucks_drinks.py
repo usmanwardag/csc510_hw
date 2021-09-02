6e52bc1156416b3e96c6a4d55e04bcc38bab004c
sbosenc <89551210+sbosenc@users.noreply.github.com>
2021-09-01 20:05:13 -0400

    Update starbucks_drinks.py

--- code/starbucks_drinks.py
+++ code/starbucks_drinks.py
@@ -3,27 +3,27 @@ import pandas as pd
 dataSet = pd.read_csv('csc510_hw/data/starbucks-menu-nutrition-drinks.csv')    
 
 #Finding drink with lowest calorie
-def find_low_calorie_drink():
+def find_low_calorie_drink(dataSet):
     drink = dataSet[dataSet.Calories == dataSet.Calories.min()]
     return drink[['Name','Calories']]
 
 #High protein drinks
-def find_high_protein_drink():
+def find_high_protein_drink(dataSet):
     drink =   dataSet['Protein']>=4
     drink = dataSet[drink]
     return drink[['Name','Protein']]
 
 #High carb drinks
-def find_high_carb_drink():
+def find_high_carb_drink(dataSet):
     drink=dataSet['Carb. (g)']>=30
     drink = dataSet[drink]
     return drink[['Name','Carb. (g)']]
 
 print('Lowest Calorie Drink')
-print(find_low_calorie_drink())
+print(find_low_calorie_drink(dataSet))
 print('High Protein Content Drink')
-print(find_high_protein_drink())
+print(find_high_protein_drink(dataSet))
 print('High Carb Drink')
-print(find_high_carb_drink())
+print(find_high_carb_drink(dataSet))
 
 
